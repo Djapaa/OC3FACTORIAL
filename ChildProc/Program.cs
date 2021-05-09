7b45f91b4ff8d08aa86edfa4a7de06a5ad03767b
Djapaa <estebmmr@mail.ru>
2021-05-09 21:40:08 +0500

    Добавьте файлы проекта.

---
+++ ChildProc/Program.cs
@@ -0,0 +1,98 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChildrenProc
+{
+    public class ДочернийПроцесс
+
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                //Console.WriteLine(args.Length.ToString() + ' ' + args[0]);
+                //Console.ReadKey();
+                Process p = Process.GetCurrentProcess();
+                //p.StartInfo.EnvironmentVariables[args[0]] = args[1];
+
+
+                // Получаем текущий процесс
+
+                //Process p = Process.GetCurrentProcess();
+
+                // Получаем блок окружения текущего процесса
+
+                var env = p.StartInfo.EnvironmentVariables;
+                double xmax = 11;
+                double xmin = 1;
+                double y = 0;
+                int fact = Convert.ToInt32(env["Функция"]);
+
+                if (Convert.ToBoolean(fact))
+                {
+                    int f = 1;
+                    for (int i = 1; i <= fact; i++)
+                        f = f * i;
+                    Console.Write("The Factorial of {0} is: {1}\n", fact, f);
+                }
+                foreach (string key in env.Keys)
+                {
+                    //Console.WriteLine(key);
+                    //Console.WriteLine(env[key]);
+                    //Console.WriteLine("key = " + key.ToString() + "  env[" + key.ToString() + "] = " + env[key].ToString());
+                    //Console.WriteLine($"!!!! key = {0},env[key] = {1} ", key, env[key]);
+                    ////Console.WriteLine(key, env.Keys);
+                    if ((key == "Выбранные функции") && (env[key] == "1 и 2"))
+                    {
+                        for (double x = xmin; x < xmax; x++)
+                        {
+                            y = Math.Sin(x) * Math.Cos(x);
+                            Console.WriteLine("Sin({0})*Сos({0}) = {1}/ key = {2},env[key] = {3} ", x, y, key, env[key]);
+
+                        }
+                    }
+                    ////if(key = int numb )
+                    //// {
+                    ////    if (env[key]=6)
+                    ////    {
+                    ////    }
+                    //// }
+                    //else if ((key == "Выбранная функция") && (env[key] == "6"))
+                    //{
+                    //    //Console.WriteLine(env.Keys.ToString());
+                    //    Console.WriteLine(key);
+                    //    Console.WriteLine(env[key]);
+                    //    Console.WriteLine($"key = {0},env[key] = {1} ", key, env[key]);
+                    //    int i, f = 1;
+                    //    int num = 2;
+                    //    Console.WriteLine($"{num}");
+                    //    for (i = 1; i <= num; i++)
+                    //    
+                    //        f = f * i;
+                    //        Console.Write("The Factorial of {0} is: {1}\n", num, f);
+                    //    }
+                    //}
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ lab3OCFact/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParentProc
+{
+    public class РодительскийПроцесс
+
+    {
+
+        public static void Main(string[] args)
+
+        {
+
+            if (args.Length != 2)
+
+            {
+                Console.WriteLine("Выберите функцию:\n1)sin(x); \n2)cos(x); \n3)x^2; \n4)e^x; \n5)ln(x);\n6)Factorial(x) \n0)Выход;\n");
+                Console.WriteLine("Использование:");
+
+                Console.WriteLine(string.Format("\t{0} [Номер выбранной ф-ции] [Номер выбранной ф-ции]", System.Reflection.Assembly.GetEntryAssembly().Location));
+
+                return;
+
+            }
+
+            double step = 1;
+            double xmax = 11;
+            double xmin = 1;
+            double y = 0;
+            for (int i = 0; i <= args.Length; i++)
+            {
+                if (args[i] == "1")
+
+                {
+                    for (double x = xmin; x < xmax; x += step)
+                    {
+                        y = Math.Sin(x);
+                        Console.WriteLine("Sin({0})={1}", x, y);
+                    }
+                }
+                else if (args[i] == "2")
+
+                {
+                    for (double x = xmin; x < xmax; x += step)
+                    {
+                        y = Math.Cos(x);
+                        Console.WriteLine("Cos({0})={1}", x, y);
+
+                    }
+
+                }
+                else if (args[i] == "3")
+
+                {
+                    for (double x = xmin; x < xmax; x += step)
+                    {
+                        y = x * x;
+                        Console.WriteLine("x^2({0})={1}", x, y);
+                    }
+                }
+                else if (args[i] == "4")
+
+                {
+                    for (double x = xmin; x < xmax; x += step)
+                    {
+                        y = Math.Exp(x);
+                        Console.WriteLine("e^({0})={1}", x, y);
+                    }
+                }
+                else if (args[i] == "5")
+                {
+                    for (double x = xmin; x < xmax; x += step)
+                    {
+                        y = Math.Log(x);
+                        Console.WriteLine("Log({0})={1}", x, y);
+                    }
+                }
+                if (i == 1)
+                    break;
+            }
+
+
+            Process myProcess = new Process();
+
+            Console.WriteLine("Началось выполнение родительского процесса");
+
+            try
+            {
+                // необходимо для возможности установки переменных окружения
+
+                myProcess.StartInfo.UseShellExecute = false;
+
+                if (((args[0] == "1") && (args[1] == "2")) || ((args[0] == "2") && (args[1] == "1")))
+                {
+                    args[0] = "Выбранные функции";
+                    args[1] = "1 и 2";
+                    //   устанавливаем значение переменной окружения
+                    //для создаваемого процесса
+
+
+                    myProcess.StartInfo.EnvironmentVariables[args[0]] = args[1];
+
+
+                }
+                // указываем имя выполняемого файла для нового процесса
+
+                else if (args[0] == "6")
+                {
+                    Console.WriteLine("введите число");
+                    string num = Console.ReadLine();
+                    args[0] = "Выбранная функция";
+                    args[1] = "6";
+
+                    myProcess.StartInfo.EnvironmentVariables[args[0]] = args[1];
+                    myProcess.StartInfo.Environment.Add("Функция", num);
+
+                }
+
+                // и запускаем его
+
+                Console.WriteLine("Дочерний процесс начался!");
+
+                myProcess.StartInfo.FileName = @"C:\Users\esteb\source\repos\lab3OCFact\ChildProc\bin\Debug\ChildProc.exe";
+
+                myProcess.Start();
+
+                // Ждём завершения дочернего процесса
+
+                myProcess.WaitForExit();
+                Console.WriteLine("Дочерний процесс завершен!");
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+
+            }
+
+        }
+
+    }
+}
